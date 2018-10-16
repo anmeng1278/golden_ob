7b61c49019896915ab9e5db8d95b0f146d4961b4
anmeng <anmeng@jsj.com.cn>
2018-10-16 17:46:36 +0800

    CartDto

---
+++ src/main/java/com/jsj/member/ob/dto/api/Cart/CartDto.java
@@ -0,0 +1,33 @@
+package com.jsj.member.ob.dto.api.Cart;
+
+import com.jsj.member.ob.dto.api.BaseDto;
+
+public class CartDto extends BaseDto {
+
+    /**
+     * 购物车id
+     */
+    private Integer cartId;
+
+    /**
+     * 用户openId
+     */
+    private String openId;
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public Integer getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Integer cartId) {
+        this.cartId = cartId;
+    }
+
+}
