4aee79958e506a916a6b249a1a5a73404aa7276c
anmeng <anmeng@jsj.com.cn>
2018-10-17 11:18:17 +0800

    修改GetCartProductRequ传输对象

--- src/main/java/com/jsj/member/ob/dto/api/cart/GetCartProductsRequ.java
+++ src/main/java/com/jsj/member/ob/dto/api/cart/GetCartProductsRequ.java
@@ -2,10 +2,14 @@ package com.jsj.member.ob.dto.api.cart;
 
 import com.jsj.member.ob.dto.BaseRequ;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GetCartProductsRequ {
 
     public  GetCartProductsRequ(){
         this.baseRequ = new BaseRequ();
+        this.cartProductDtos = new ArrayList<>();
     }
     private BaseRequ baseRequ;
 
@@ -16,5 +20,18 @@ public class GetCartProductsRequ {
 
     public void setBaseRequ(BaseRequ baseRequ) {
         this.baseRequ = baseRequ;
+    }
+
+    /**
+     * 购物车商品列表
+     */
+    private List<CartProductDto> cartProductDtos;
+
+    public List<CartProductDto> getCartProductDtos() {
+        return cartProductDtos;
+    }
+
+    public void setCartProductDtos(List<CartProductDto> cartProductDtos) {
+        this.cartProductDtos = cartProductDtos;
     }
 }
