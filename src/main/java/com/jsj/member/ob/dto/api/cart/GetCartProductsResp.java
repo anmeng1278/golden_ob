8e0714dc617d2d127c42365354fb691217ec46fd
anmeng <anmeng@jsj.com.cn>
2018-10-17 11:18:30 +0800

    修改GetCartProductsResp

--- src/main/java/com/jsj/member/ob/dto/api/cart/GetCartProductsResp.java
+++ src/main/java/com/jsj/member/ob/dto/api/cart/GetCartProductsResp.java
@@ -1,17 +1,51 @@
 package com.jsj.member.ob.dto.api.cart;
 
+import com.jsj.member.ob.dto.api.product.Product;
+import io.swagger.models.auth.In;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class GetCartProductsResp {
 
-    public GetCartProductsResp(){
-        this.cartProductDtos = new ArrayList<>();
+    private Integer carProductId;
+
+    private Integer productId;
+
+    private String openId;
+
+    private Integer number;
+
+    public Integer getCarProductId() {
+        return carProductId;
     }
 
-    /**
-     * 购物车商品列表
-     */
-    private List<com.jsj.member.ob.dto.api.Cart.CartProductDto> cartProductDtos;
+    public void setCarProductId(Integer carProductId) {
+        this.carProductId = carProductId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 
 }
