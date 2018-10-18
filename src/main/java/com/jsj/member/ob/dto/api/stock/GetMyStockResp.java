ef4151dce695bd10c0a66d07ceb820efb0919012
anmeng <anmeng@jsj.com.cn>
2018-10-18 09:32:04 +0800

    修改GetMyStockResp

--- src/main/java/com/jsj/member/ob/dto/api/stock/GetMyStockResp.java
+++ src/main/java/com/jsj/member/ob/dto/api/stock/GetMyStockResp.java
@@ -1,54 +1,18 @@
 package com.jsj.member.ob.dto.api.stock;
 
+import java.util.List;
+
 public class GetMyStockResp {
 
-    private Integer stockId;
+    List<StockDto> stockDtoList;
 
-    private String openId;
-
-    private Integer productId;
-
-    private Integer orderId;
-
-    private Integer number;
-
-    public Integer getNumber() {
-        return number;
+    public List<StockDto> getStockDtoList() {
+        return stockDtoList;
     }
 
-    public void setNumber(Integer number) {
-        this.number = number;
+    public void setStockDtoList(List<StockDto> stockDtoList) {
+        this.stockDtoList = stockDtoList;
     }
 
-    public Integer getStockId() {
-        return stockId;
-    }
 
-    public void setStockId(Integer stockId) {
-        this.stockId = stockId;
-    }
-
-    public String getOpenId() {
-        return openId;
-    }
-
-    public void setOpenId(String openId) {
-        this.openId = openId;
-    }
-
-    public Integer getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public Integer getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Integer orderId) {
-        this.orderId = orderId;
-    }
 }
