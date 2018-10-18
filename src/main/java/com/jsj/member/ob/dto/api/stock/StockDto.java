51edd6f91635c56249d3bfa3a2bf73b2afae05a3
anmeng <anmeng@jsj.com.cn>
2018-10-18 09:32:40 +0800

    添加StockDto

---
+++ src/main/java/com/jsj/member/ob/dto/api/stock/StockDto.java
@@ -0,0 +1,66 @@
+package com.jsj.member.ob.dto.api.stock;
+
+import com.jsj.member.ob.dto.api.product.ProductDto;
+
+public class StockDto {
+
+    private Integer stockId;
+
+    private String openId;
+
+    private Integer productId;
+
+    private Integer orderId;
+
+    private Integer number;
+
+    private ProductDto productDto;
+
+    public ProductDto getProductDto() {
+        return productDto;
+    }
+
+    public void setProductDto(ProductDto productDto) {
+        this.productDto = productDto;
+    }
+
+    public Integer getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(Integer stockId) {
+        this.stockId = stockId;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+}
