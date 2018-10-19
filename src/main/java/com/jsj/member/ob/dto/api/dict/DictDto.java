d4fd0b450d26147ee0bd8d796f91ad95955a8736
anmeng <anmeng@jsj.com.cn>
2018-10-19 15:22:39 +0800

    添加DictDto对象

---
+++ src/main/java/com/jsj/member/ob/dto/api/dict/DictDto.java
@@ -0,0 +1,29 @@
+package com.jsj.member.ob.dto.api.dict;
+
+public class DictDto {
+    /**
+     * 字典id
+     */
+    private int dictId;
+
+    /**
+     * 名称
+     */
+    private String dictName;
+
+    public int getDictId() {
+        return dictId;
+    }
+
+    public void setDictId(int dictId) {
+        this.dictId = dictId;
+    }
+
+    public String getDictName() {
+        return dictName;
+    }
+
+    public void setDictName(String dictName) {
+        this.dictName = dictName;
+    }
+}
