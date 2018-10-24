778b60ef3f9abd7335d876f95a86682f65a67e37
anmeng <anmeng@jsj.com.cn>
2018-10-24 16:15:14 +0800

    添加搜索用户功能

--- src/main/java/com/jsj/member/ob/controller/admin/AdminWechatController.java
+++ src/main/java/com/jsj/member/ob/controller/admin/AdminWechatController.java
@@ -2,10 +2,13 @@ package com.jsj.member.ob.controller.admin;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
+import com.jsj.member.ob.entity.Dict;
 import com.jsj.member.ob.entity.Wechat;
+import com.jsj.member.ob.enums.DictType;
 import com.jsj.member.ob.enums.WechatType;
 import com.jsj.member.ob.service.WechatService;
 import com.jsj.member.ob.utils.CCPage;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,25 +29,31 @@ public class AdminWechatController {
     private WechatService wechatService;
 
     /**
-     * 查询所有用户列表
-     * @param page 当前页
-     * @param limit 每页显示条数
+     * 查询用户列表
+     * @param page
+     * @param limit
+     * @param keys
      * @param model
      * @return
      */
-
     @GetMapping(value = {"","/index"})
     public String index(@RequestParam(value = "page", defaultValue = "1") Integer page,
-                        @RequestParam(value = "limit", defaultValue = "10") Integer limit,Model model) {
-        Page<Wechat> pageInfo = new Page<>(page, limit);
-        Page<Wechat> pp = wechatService.selectPage(pageInfo, new EntityWrapper<Wechat>().where("delete_time is null"));
+                        @RequestParam(value = "limit", defaultValue = "10") Integer limit,
+                        @RequestParam(value = "keys", defaultValue = "") String keys,Model model) {
 
-        //遍历枚举得出来源
-        List<WechatType> wechatTypes = Arrays.asList(WechatType.values());
+        EntityWrapper<Wechat> wrapper = new EntityWrapper<>();
+        wrapper.where("delete_time is null");
+        wrapper.where(!StringUtils.isBlank(keys), "(nickname LIKE concat(concat('%',{0}),'%') )", keys);
+        wrapper.orderBy("create_time desc");
+
+        Page<Wechat> pageInfo = new Page<>(page, limit);
+        Page<Wechat> pp = wechatService.selectPage(pageInfo,wrapper);
+
+        model.addAttribute("keys", keys);
         model.addAttribute("infos", new CCPage<Wechat>(pp, limit));
-        model.addAttribute("wechatTypes", wechatTypes);
 
         return "admin/wechat/index";
+
     }
 
 }
