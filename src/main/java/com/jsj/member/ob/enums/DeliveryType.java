8b6dfa50f88c8e9d22f869603b22af3cbfe8ec9a
anmeng <anmeng@jsj.com.cn>
2018-10-26 17:41:21 +0800

    添加配送类型状态

---
+++ src/main/java/com/jsj/member/ob/enums/DeliveryType.java
@@ -0,0 +1,41 @@
+package com.jsj.member.ob.enums;
+
+import com.jsj.member.ob.exception.FatalException;
+
+public enum DeliveryType {
+
+    //1：自提，2：配送'
+
+    PICKUP(1, "自提"),
+
+    DISTRIBUTE(2, "配送");
+
+
+    private Integer value;
+    private String message;
+
+    DeliveryType(Integer value, String message) {
+        this.value = value;
+        this.message = message;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public static DeliveryType valueOf(int value) {
+        switch (value) {
+            case 1:
+                return PICKUP;
+            case 2:
+                return DISTRIBUTE;
+            default:
+                throw new FatalException("未知的枚举值");
+        }
+    }
+
+}
