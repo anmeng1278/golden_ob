31249e104023e5c6fb8a4c0e29b1c7b51ccf8f9b
anmeng <anmeng@jsj.com.cn>
2018-10-30 16:15:14 +0800

    修改配送controller

--- src/main/java/com/jsj/member/ob/controller/admin/AdminDeliveryController.java
+++ src/main/java/com/jsj/member/ob/controller/admin/AdminDeliveryController.java
@@ -2,19 +2,20 @@ package com.jsj.member.ob.controller.admin;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
+import com.jsj.member.ob.dto.RestResponseBo;
 import com.jsj.member.ob.entity.*;
-import com.jsj.member.ob.enums.ActivityType;
-import com.jsj.member.ob.enums.DeliveryStatus;
-import com.jsj.member.ob.enums.DeliveryType;
-import com.jsj.member.ob.enums.OrderStatus;
+import com.jsj.member.ob.enums.*;
+import com.jsj.member.ob.logic.DictLogic;
 import com.jsj.member.ob.service.*;
 import com.jsj.member.ob.utils.CCPage;
+import com.jsj.member.ob.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 
@@ -73,7 +74,41 @@ public class AdminDeliveryController {
         return "admin/delivery/index";
     }
 
+    @GetMapping("/send/{deliveryId}")
+    public String sendInfo(@PathVariable("deliveryId") Integer deliveryId, Model model){
+            Delivery delivery = new Delivery();
+            if (deliveryId > 0) {
+                delivery = deliveryService.selectById(deliveryId);
+            }
+            model.addAttribute("info", delivery);
+            model.addAttribute("deliveryId", deliveryId);
+            return "admin/delivery/info";
 
+    }
+
+    /**
+     * 修改状态
+     *
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/status", method = RequestMethod.POST)
+    @ResponseBody
+    public RestResponseBo status(HttpServletRequest request) {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        String method = request.getParameter("method");
+
+        Delivery delivery = deliveryService.selectById(id);
+
+        if (method.equals("ifpass")) {
+        }
+        if (method.equals("send")) {
+        }
+
+        return RestResponseBo.ok("操作成功");
+
+    }
 
 
 }
