fec0cb145c9ad2e0c4ba566db2f324a91a235f47
anmeng <anmeng@jsj.com.cn>
2018-10-31 10:48:45 +0800

    接口对接测试类

--- src/main/java/com/jsj/member/ob/logic/wechatApiLogic.java
+++ src/main/java/com/jsj/member/ob/logic/wechatApiLogic.java
@@ -2,8 +2,7 @@ package com.jsj.member.ob.logic;
 
 import com.alibaba.fastjson.JSON;
 import com.jsj.member.ob.config.Webconfig;
-import com.jsj.member.ob.dto.wechatApi.GetAccessTokenRequ;
-import com.jsj.member.ob.dto.wechatApi.GetAccessTokenResp;
+import com.jsj.member.ob.dto.wechatApi.*;
 import com.jsj.member.ob.utils.HttpUtils;
 import com.jsj.member.ob.utils.Md5Utils;
 import org.apache.commons.collections4.map.LinkedMap;
@@ -27,14 +26,16 @@ public class WechatApiLogic {
         wechatApiLogic.webconfig = this.webconfig;
     }
 
+    /**
+     * 获取AccessToken
+     * @param requ
+     * @return
+     */
     public static GetAccessTokenResp GetAccessToken(GetAccessTokenRequ requ) {
 
         GetAccessTokenResp resp = new GetAccessTokenResp();
 
         try {
-
-            System.out.println( wechatApiLogic.webconfig.getJsApiTicketUrl());
-            System.out.println( wechatApiLogic.webconfig.getImgServerURL());
 
             LinkedMap<String, String> map = new LinkedMap<>();
             map.put("TimeStamp", requ.getRequestHead().getTimeStamp());
@@ -57,189 +58,122 @@ public class WechatApiLogic {
 
     }
 
+    /**
+     * 获取公众号的 jsapi_ticket
+     * @param requ
+     * @return
+     */
+    public static GetJsApiTicketResp GetJsApiTicket(GetJsApiTicketRequ requ) {
 
-    //static BaseRequest baseRequest = null;
-    //static RequestHead requestHead = null;
-    //static RequestBody requestBody = null;
-    //static Map<String, String> map = null;
-    //
-    //
-    ////获取 access_token
-    //public static String GetAccessToken(String url) {
-    //    String result = "";
-    //    try {
-    //        if (url.equals(wechatApiLogic.GetProperties().getProperty("accessTokenUrl"))) {
-    //
-    //            BaseRequest baseRequest = wechatApiLogic.GetBaseRequest(1);
-    //
-    //        } else if (url.equals(wechatApiLogic.GetProperties().getProperty("jsApiTicketUrl"))) {
-    //
-    //            BaseRequest baseRequest = wechatApiLogic.GetBaseRequest(2);
-    //
-    //        } else if (url.equals(wechatApiLogic.GetProperties().getProperty("weChatAccessTokenUrl"))) {
-    //
-    //            BaseRequest baseRequest = wechatApiLogic.GetBaseRequest(3);
-    //
-    //        } else if (url.equals(wechatApiLogic.GetProperties().getProperty("payTradeUrl"))) {
-    //
-    //            BaseRequest baseRequest = wechatApiLogic.GetBaseRequest(4);
-    //
-    //        }
-    //        result = wechatApiLogic.DoPost(url, JSON.toJSONString(baseRequest), "application/json");
-    //        System.out.println(result);
-    //    } catch (Exception e) {
-    //        return null;
-    //    }
-    //    return result;
-    //}
-    //
-    ////微信公众号支付
-    //public static String GetPayTrade(String url, String orderId) {
-    //    baseRequest = new BaseRequest();
-    //    requestHead = new RequestHead();
-    //    requestBody = new RequestBody();
-    //    map = new LinkedHashMap<>();
-    //    String result = "";
-    //
-    //    Order order = OrderLogic.orderLogic.orderService.selectOne(new EntityWrapper<Order>().where("order_id={0}", orderId));
-    //    try {
-    //
-    //        Properties prop = wechatApiLogic.GetProperties();
-    //        requestHead.setTimeStamp(prop.getProperty("TimeStamp"));
-    //        requestHead.setSourceFrom(prop.getProperty("SourceFrom"));
-    //
-    //        requestBody.setPlatformAppId(prop.getProperty("PlatformAppId"));
-    //        requestBody.setPlatformToken(prop.getProperty("PlatformToken"));
-    //        requestBody.setSourceWay(prop.getProperty("SourceWay"));
-    //        requestBody.setSourceApp(prop.getProperty("SourceApp"));
-    //        requestBody.setPayMethod(prop.getProperty("PayMethod"));
-    //        requestBody.setOutTradeId(orderId);
-    //        requestBody.setPayAmount(String.valueOf(order.getPayAmount()));
-    //        requestBody.setOpenId(order.getOpenId());
-    //        requestBody.setOrderTimeOut(String.valueOf(order.getExpiredTime()));
-    //
-    //        map.put("TimeStamp", prop.getProperty("TimeStamp"));
-    //        map.put("SourceFrom", prop.getProperty("SourceFrom"));
-    //        map.put("PlatformAppId", requestBody.getPlatformAppId());
-    //        map.put("PlatformToken", requestBody.getPlatformToken());
-    //        map.put("SourceWay", requestBody.getSourceWay());
-    //        map.put("SourceApp", requestBody.getSourceApp());
-    //        map.put("PayMethod", requestBody.getPayMethod());
-    //        map.put("OutTradeId", orderId);
-    //        map.put("PayAmount", String.valueOf(order.getPayAmount()));
-    //        map.put("OpenId", order.getOpenId());
-    //        map.put("OrderTimeOut", String.valueOf(order.getExpiredTime()));
-    //
-    //        requestHead.setSign(wechatApiLogic.GetMd5(map, prop.getProperty("token")).toLowerCase());
-    //        baseRequest.setRequestBody(requestBody);
-    //        baseRequest.setRequestHead(requestHead);
-    //
-    //        result = wechatApiLogic.DoPost(url, JSON.toJSONString(baseRequest), "application/json");
-    //        System.out.println(result);
-    //    } catch (Exception e) {
-    //        return null;
-    //    }
-    //    return result;
-    //}
-    //
-    //
-    ///**
-    // * 获得配置文件
-    // *
-    // * @return
-    // * @throws IOException
-    // */
-    //public static Properties GetProperties() throws IOException {
-    //    String fileName = "application.yml";
-    //    Properties prop = new Properties();// 属性集合对象
-    //    //获取文件流
-    //    InputStream fis = wechatApiLogic.class.getClassLoader().getResourceAsStream(fileName);
-    //    prop.load(fis);
-    //    fis.close();
-    //    return prop;
-    //}
-    //
-    //
-    ///**
-    // * 获得请求体对象
-    // *
-    // * @return
-    // * @throws IOException
-    // */
-    //public static BaseRequest GetBaseRequest(int type) throws IOException {
-    //    baseRequest = new BaseRequest();
-    //    requestHead = new RequestHead();
-    //    requestBody = new RequestBody();
-    //    map = new LinkedHashMap<>();
-    //    Properties prop = wechatApiLogic.GetProperties();
-    //    requestHead.setTimeStamp(prop.getProperty("TimeStamp"));
-    //    requestHead.setSourceFrom(prop.getProperty("SourceFrom"));
-    //
-    //    if (type == 1) {
-    //        map.put("TimeStamp", prop.getProperty("TimeStamp"));
-    //        map.put("SourceFrom", prop.getProperty("SourceFrom"));
-    //    }
-    //    if (type == 2) {
-    //        map.put("TimeStamp", prop.getProperty("TimeStamp"));
-    //        map.put("SourceFrom", prop.getProperty("SourceFrom"));
-    //        map.put("AccessToken", prop.getProperty("AccessToken"));
-    //        requestBody.setAccessToken(prop.getProperty("AccessToken"));
-    //    }
-    //    if (type == 3) {
-    //        map.put("TimeStamp", prop.getProperty("TimeStamp"));
-    //        map.put("SourceFrom", prop.getProperty("SourceFrom"));
-    //        map.put("Code", prop.getProperty("Code"));
-    //        requestBody.setCode(prop.getProperty("Code"));
-    //    }
-    //
-    //    requestHead.setSign(wechatApiLogic.GetMd5(map, prop.getProperty("token")).toLowerCase());
-    //    baseRequest.setRequestBody(requestBody);
-    //    baseRequest.setRequestHead(requestHead);
-    //    return baseRequest;
-    //}
-    //
-    ///**
-    // * 处理post请求
-    // *
-    // * @param url
-    // * @param params
-    // * @param contentType
-    // * @return
-    // */
-    //public static String DoPost(String url, String params, String contentType) {
-    //    CloseableHttpClient httpclient = HttpClients.createDefault();
-    //    HttpPost httpPost = new HttpPost(url);// 创建httpPost
-    //    httpPost.setHeader("Content-Type", contentType);
-    //    String charSet = "UTF-8";
-    //    StringEntity entity = new StringEntity(params, charSet);
-    //    httpPost.setEntity(entity);
-    //    CloseableHttpResponse response = null;
-    //    String jsonString = null;
-    //    try {
-    //        response = httpclient.execute(httpPost);
-    //        StatusLine status = response.getStatusLine();
-    //        int state = status.getStatusCode();
-    //        if (state == HttpStatus.SC_OK) {
-    //            HttpEntity responseEntity = response.getEntity();
-    //            jsonString = EntityUtils.toString(responseEntity);
-    //        }
-    //        if (response != null) {
-    //            response.close();
-    //        }
-    //        httpclient.close();
-    //    } catch (Exception e) {
-    //        jsonString = null;
-    //    }
-    //    return jsonString;
-    //}
-    //
-    ///**
-    // * md5加密
-    // *
-    // * @param params
-    // * @param secret
-    // * @return
-    // */
+        GetJsApiTicketResp resp = new GetJsApiTicketResp();
+
+        try {
+
+            LinkedMap<String, String> map = new LinkedMap<>();
+            map.put("TimeStamp", requ.getRequestHead().getTimeStamp());
+            map.put("SourceFrom", requ.getRequestHead().getSourceFrom());
+            map.put("AccessToken",requ.getRequestBody().getAccessToken());
+
+            String sign = WechatApiLogic.GetMd5str(map, "#wugf543sxcv5*$#");
+            sign = Md5Utils.MD5(sign);
+            requ.getRequestHead().setSign(sign);
+
+            //设置签名
+            String result = HttpUtils.json(wechatApiLogic.webconfig.getJsApiTicketUrl(), JSON.toJSONString(requ));
+            resp = JSON.parseObject(result, GetJsApiTicketResp.class);
+
+        } catch (Exception ex) {
+            resp.getResponseHead().setCode("0001");
+            resp.getResponseHead().setMessage(ex.getMessage());
+        }
+
+        return resp;
+
+    }
+
+
+    /**
+     * 通过 code 换取网页授权 access_token
+     * @param requ
+     * @return
+     */
+    public static GetWeChatAccessTokenResp GetWeChatAccessToken(GetWeChatAccessTokenRequ requ) {
+
+        GetWeChatAccessTokenResp resp = new GetWeChatAccessTokenResp();
+
+        try {
+
+            LinkedMap<String, String> map = new LinkedMap<>();
+            map.put("TimeStamp", requ.getRequestHead().getTimeStamp());
+            map.put("SourceFrom", requ.getRequestHead().getSourceFrom());
+            map.put("Code",requ.getRequestBody().getCode());
+
+            String sign = WechatApiLogic.GetMd5str(map, "#wugf543sxcv5*$#");
+            sign = Md5Utils.MD5(sign);
+            requ.getRequestHead().setSign(sign);
+
+            //设置签名
+            String result = HttpUtils.json(wechatApiLogic.webconfig.getWeChatAccessTokenUrl(), JSON.toJSONString(requ));
+            resp = JSON.parseObject(result, GetWeChatAccessTokenResp.class);
+
+        } catch (Exception ex) {
+            resp.getResponseHead().setCode("0001");
+            resp.getResponseHead().setMessage(ex.getMessage());
+        }
+
+        return resp;
+
+    }
+
+
+    /**
+     * 微信公众号下单接口，使用返回参数调起微信客户端支付
+     * @param requ
+     * @return
+     */
+    public static GetPayTradeResp GetPayTrade(GetPayTradeRequ requ) {
+
+        GetPayTradeResp resp = new GetPayTradeResp();
+
+        try {
+
+            LinkedMap<String, String> map = new LinkedMap<>();
+            map.put("TimeStamp", requ.getRequestHead().getTimeStamp());
+            map.put("SourceFrom", requ.getRequestHead().getSourceFrom());
+            map.put("PlatformAppId",wechatApiLogic.webconfig.getPlatformAppId());
+            map.put("PlatformToken", wechatApiLogic.webconfig.getPlatformToken());
+            map.put("SourceWay", requ.getRequestBody().getSourceWay());
+            map.put("SourceApp", requ.getRequestBody().getSourceApp());
+            map.put("PayMethod", requ.getRequestBody().getPayAmount());
+            map.put("OutTradeId",requ.getRequestBody().getOutTradeId());
+            map.put("PayAmount", requ.getRequestBody().getPayAmount());
+            map.put("OpenId", requ.getRequestBody().getOpenId());
+            map.put("OrderTimeOut", requ.getRequestBody().getOrderTimeOu());
+
+            String sign = WechatApiLogic.GetMd5str(map, "#wugf543sxcv5*$#");
+            sign = Md5Utils.MD5(sign);
+            requ.getRequestHead().setSign(sign);
+
+            //设置签名
+            String result = HttpUtils.json(wechatApiLogic.webconfig.getWeChatAccessTokenUrl(), JSON.toJSONString(requ));
+            resp = JSON.parseObject(result, GetPayTradeResp.class);
+
+        } catch (Exception ex) {
+            resp.getResponseHead().setCode("0001");
+            resp.getResponseHead().setMessage(ex.getMessage());
+        }
+
+        return resp;
+
+    }
+
+
+    /**
+     * 签名字符串处理
+     * @param map
+     * @param secret
+     * @return
+     */
     private static String GetMd5str(LinkedMap<String, String> map, String secret) {
         StringBuilder sb = new StringBuilder();
         try {
--- src/test/java/com/jsj/member/ob/logic/wechatApiLogicTest.java
+++ src/test/java/com/jsj/member/ob/logic/wechatApiLogicTest.java
@@ -2,8 +2,7 @@ package com.jsj.member.ob.logic;
 
 import com.alibaba.fastjson.JSON;
 import com.jsj.member.ob.App;
-import com.jsj.member.ob.dto.wechatApi.GetAccessTokenRequ;
-import com.jsj.member.ob.dto.wechatApi.GetAccessTokenResp;
+import com.jsj.member.ob.dto.wechatApi.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +15,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 public class wechatApiLogicTest {
 
+    //获取token
     @Test
     public void getAccessToken() {
 
@@ -24,11 +24,51 @@ public class wechatApiLogicTest {
         System.out.println(JSON.toJSONString(resp));
     }
 
+    //获取ticket
     @Test
-    public void testService() {
-        GetAccessTokenRequ requ = new GetAccessTokenRequ();
-        requ.getRequestHead().setSign("123");
+    public void getJsApiTicket() {
 
-        System.out.println(JSON.toJSONString(requ));
+        //获取accessToken
+        GetAccessTokenRequ tokenRequ = new GetAccessTokenRequ();
+        GetAccessTokenResp tokenResp = WechatApiLogic.GetAccessToken(tokenRequ);
+        String accessToken = tokenResp.getResponseBody().getAccessToken();
+
+        GetJsApiTicketRequ ticketRequ = new GetJsApiTicketRequ();
+        ticketRequ.getRequestBody().setAccessToken(accessToken);
+        GetJsApiTicketResp ticketResp = WechatApiLogic.GetJsApiTicket(ticketRequ);
+        System.out.println(JSON.toJSONString(ticketResp));
     }
+
+
+    //通过code获取网页授权access_token
+    @Test
+    public void getWeChatAccessToken() {
+
+
+        GetWeChatAccessTokenRequ weChatRequ = new GetWeChatAccessTokenRequ();
+        weChatRequ.getRequestBody().setCode("021AxV5w1vUDw90vTY5w991I2S5w1AxV5x");
+
+        GetWeChatAccessTokenResp weChatResp = WechatApiLogic.GetWeChatAccessToken(weChatRequ);
+        System.out.println(JSON.toJSONString(weChatResp));
+    }
+
+    @Test
+    public void getPayTrade() {
+
+
+        GetPayTradeRequ requ = new GetPayTradeRequ();
+        requ.getRequestBody().setPlatformAppId(WechatApiLogic.wechatApiLogic.webconfig.getPlatformAppId());
+        requ.getRequestBody().setPlatformToken(WechatApiLogic.wechatApiLogic.webconfig.getPlatformToken());
+        requ.getRequestBody().setOpenId("oeQDZtzvDoD-0mTmyJH6rGAX8odc");
+        requ.getRequestBody().setSourceWay("20");
+        requ.getRequestBody().setPayAmount("0.1");
+        requ.getRequestBody().setPayMethod("22");
+        requ.getRequestBody().setOutTradeId("9534634332");
+        requ.getRequestBody().setOrderTimeOu("20180328173342");
+        requ.getRequestBody().setSourceApp("600");
+
+        GetPayTradeResp resp = WechatApiLogic.GetPayTrade(requ);
+        System.out.println(JSON.toJSONString(resp));
+    }
+
 }
