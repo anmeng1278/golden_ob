6c78699816e17e82c78f0970ef2281448a8cc408
anmeng <anmeng@jsj.com.cn>
2018-12-12 18:32:23 +0800

    添加订单控制类

--- src/main/java/com/jsj/member/ob/controller/index/OrderController.java
+++ src/main/java/com/jsj/member/ob/controller/index/OrderController.java
@@ -1,12 +1,19 @@
 package com.jsj.member.ob.controller.index;
 
 import com.jsj.member.ob.controller.BaseController;
+import com.jsj.member.ob.dto.RestResponseBo;
+import com.jsj.member.ob.dto.api.order.OrderDto;
+import com.jsj.member.ob.dto.api.order.OrderProductDto;
+import com.jsj.member.ob.enums.OrderFlag;
+import com.jsj.member.ob.logic.OrderLogic;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @ApiIgnore
 @Controller
@@ -17,14 +24,27 @@ public class OrderController extends BaseController {
      * 订单列表
      *
      * @param request
-     * @return TODO 显示未支付、已支付的订单
+     * @return
      */
-    @GetMapping(value = {"/order"})
-    public String order(HttpServletRequest request) {
+    @GetMapping(value = {"/order/{orderFlag}"})
+    public String order(@PathVariable("orderFlag") int orderFlag, HttpServletRequest request) {
 
         String openId = this.OpenId();
 
-        return "index/order";
+        List<OrderDto> orderDtos = OrderLogic.GetMyOrder(openId, orderFlag);
+
+        request.setAttribute("orderDtos",orderDtos);
+        request.setAttribute("orderFlag",orderFlag);
+
+        return "index/OrderList";
+    }
+
+    @GetMapping(value = {"/cancelOrder/{orderId}"})
+    public String cancelOrder(@PathVariable("orderId") int orderId, HttpServletRequest request){
+
+        OrderLogic.CancelOrder(orderId);
+
+        return "redirect:index/OrderList/"+orderId;
     }
 
 }
