1877ef6b3ea505b1daa724ae7e95bec25a595c0a
cc <52334755@qq.com>
2018-12-21 14:29:22 +0800

    添加全局异常

--- src/main/java/com/jsj/member/ob/config/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/jsj/member/ob/config/GlobalDefaultExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.jsj.member.ob.config;
 
 import com.jsj.member.ob.dto.RestResponseBo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,9 +12,15 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class GlobalDefaultExceptionHandler {
 
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalDefaultExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public RestResponseBo defaultExceptionHander(HttpServletRequest request, Exception e) throws Exception {
+
+        e.printStackTrace();
+        logger.error("", e);
 
         return RestResponseBo.fail(e.getMessage());
 
