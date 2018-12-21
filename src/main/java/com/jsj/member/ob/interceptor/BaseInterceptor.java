f2487fdd2d8db891a74eb30c0e9cd9b0ba015e32
cc <52334755@qq.com>
2018-12-21 10:33:49 +0800

    修改分享链接

--- src/main/java/com/jsj/member/ob/interceptor/BaseInterceptor.java
+++ src/main/java/com/jsj/member/ob/interceptor/BaseInterceptor.java
@@ -59,7 +59,7 @@ public class BaseInterceptor implements HandlerInterceptor {
      * @return
      */
     public String Url(String url, boolean enabledCache) {
-        String format = "%s/static%s";
+        String format = "%s%s";
         url = String.format(format, webconfig.getVirtualPath(), url);
         if (enabledCache) {
             int timeStamp = DateUtils.getCurrentUnixTime();
