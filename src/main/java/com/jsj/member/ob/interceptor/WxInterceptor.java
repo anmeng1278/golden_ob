171bc36dc41d47d221babd67f35f0f974c25c387
cc <52334755@qq.com>
2018-12-25 16:19:33 +0800

    调整分享功能

--- src/main/java/com/jsj/member/ob/interceptor/WxInterceptor.java
+++ src/main/java/com/jsj/member/ob/interceptor/WxInterceptor.java
@@ -5,7 +5,6 @@ import com.jsj.member.ob.dto.thirdParty.GetAccessTokenRequ;
 import com.jsj.member.ob.dto.thirdParty.GetAccessTokenResp;
 import com.jsj.member.ob.logic.ThirdPartyLogic;
 import com.jsj.member.ob.logic.WechatLogic;
-import com.jsj.member.ob.utils.DateUtils;
 import com.jsj.member.ob.utils.SpringContextUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +12,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import weixin.popular.api.SnsAPI;
-import weixin.popular.api.TicketAPI;
 import weixin.popular.api.UserAPI;
 import weixin.popular.bean.sns.SnsToken;
-import weixin.popular.bean.ticket.Ticket;
 import weixin.popular.bean.user.User;
-import weixin.popular.util.JsUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.UUID;
 
 /**
  * 微信授权拦截
@@ -124,6 +119,7 @@ public class WxInterceptor extends HandlerInterceptorAdapter {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object o, ModelAndView modelAndView) throws Exception {
 
+        /*
         GetAccessTokenResp getAccessTokenResp = null;
 
         if (SpringContextUtils.getActiveProfile().equals("dev")) {
@@ -148,7 +144,7 @@ public class WxInterceptor extends HandlerInterceptorAdapter {
         request.setAttribute("timestamp", timestamp);
         request.setAttribute("noncestr", noncestr);
         request.setAttribute("virtualPath", webconfig.getVirtualPath());
-
+        */
 
     }
 }
