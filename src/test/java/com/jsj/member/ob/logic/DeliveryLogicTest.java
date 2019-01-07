219873aadfcf22e19a0da0a135f0f0ab01ffd3d9
张宁 <zhangning1@jsj.com.cn>
2019-01-07 11:36:49 +0800

    Master

--- src/test/java/com/jsj/member/ob/logic/DeliveryLogicTest.java
+++ src/test/java/com/jsj/member/ob/logic/DeliveryLogicTest.java
@@ -2,18 +2,25 @@ package com.jsj.member.ob.logic;
 
 import com.jsj.member.ob.App;
 import com.jsj.member.ob.dto.api.delivery.DeliveryDto;
+import com.jsj.member.ob.dto.api.stock.StockDto;
+import com.jsj.member.ob.rabbitmq.wx.TemplateDto;
+import com.jsj.member.ob.rabbitmq.wx.WxSender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = App.class)
 @WebAppConfiguration
 public class DeliveryLogicTest {
+    @Autowired
+    WxSender wxSender;
 
     @Test
     public void getDeliveryOrder() {
@@ -24,5 +31,13 @@ public class DeliveryLogicTest {
     public void getMyDelivery(){
         List<DeliveryDto> deliveryDtos = DeliveryLogic.GetDelivery("111");
         deliveryDtos.stream().forEach(s->s.getDeliveryId());
+    }
+
+    @Test
+    public void test(){
+        List<StockDto> stockDtos = DeliveryLogic.GetDeliveryStock(1);
+
+        Map map = TemplateDto.GetProduct(stockDtos);
+        System.out.println(map.get("productPrice"));
     }
 }
