9bb052fb86235dbaeb59917290ae77fb408aa64a
cc <52334755@qq.com>
2019-01-30 14:02:21 +0800

    修改开通plusBug

--- src/main/java/com/jsj/member/ob/config/SwaggerConfig.java
+++ src/main/java/com/jsj/member/ob/config/SwaggerConfig.java
@@ -7,21 +7,31 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.service.Contact;
+
+import javax.servlet.ServletContext;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
+    public Docket api(ServletContext servletContext) {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .pathMapping("/")
+                .pathProvider(new RelativePathProvider(servletContext) {
+                    @Override
+                    public String getDocumentationPath() {
+                        return "/aa/" + super.getDocumentationPath();
+                    }
+                })
                 .select() // 选择那些路径和api会生成document
+
                 .apis(RequestHandlerSelectors.any())// 对所有api进行监控
                 //不显示错误的接口地址
                 .paths(Predicates.not(PathSelectors.regex("/error.*")))//错误路径不监控
@@ -30,9 +40,9 @@ public class SwaggerConfig {
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("这是我的接口文档")
+        return new ApiInfoBuilder().title("金色严选小程序API文档")
                 .contact(new Contact("接口组", "", "zhangning1@jsj.com.cn"))
-                .description("这是SWAGGER_2生成的接口文档")
+                .description("金色严选小程序API文档")
                 .termsOfServiceUrl("NO terms of service")
                 .license("The Apache License, Version 2.0")
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
