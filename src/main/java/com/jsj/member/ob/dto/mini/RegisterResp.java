7d3818ed7c8a48e83cf62c0432eaf77936a55ae7
cc <52334755@qq.com>
2019-01-30 18:08:32 +0800

    小程序注册接口

---
+++ src/main/java/com/jsj/member/ob/dto/mini/RegisterResp.java
@@ -0,0 +1,14 @@
+package com.jsj.member.ob.dto.mini;
+
+public class RegisterResp {
+
+    private String openId;
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+}
